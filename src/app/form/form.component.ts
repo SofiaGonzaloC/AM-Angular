4548377a4d22d73fbe7b0defcf7657011a639bc7
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-17 17:58:15 -0600

    Loader and css added to form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import SignaturePad from 'signature_pad';
+
+import { User } from '../interfaces/User';
+import { UsersService } from '../service/users.service';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +12,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+
+  @ViewChild('signaturePad', { static: false }) SignaturePad: any;
+  private signaturePad: any;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private UsersService: UsersService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public form: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(200)]],
+    birthdate: ['', [Validators.required, Validators.maxLength(200)]],
+    email: ['', [Validators.required, Validators.maxLength(200), Validators.email]],
+    phone: ['', [Validators.required, Validators.maxLength(10)]],
+    phoneExtension: ['', [Validators.required, Validators.maxLength(10)]],
+  })
+
+  ngAfterViewInit() {
+    const canvas = this.SignaturePad.nativeElement;
+    this.signaturePad = new SignaturePad(canvas, {
+      backgroundColor: 'rgb(255, 255, 255)',
+      penColor: 'rgb(0, 0, 0)'
+    });
+  }
+
+
+  /**
+   * Method to save new user information
+   */
+  saveUser() {
+    const nameControl = this.form.get('name');
+    const birthdateControl = this.form.get('birthdate');
+    const emailControl = this.form.get('email');
+    const phoneControl = this.form.get('phone');
+    const phoneExtension = this.form.get('phoneExtension');
+
+    if (nameControl && birthdateControl && emailControl && phoneControl) {
+      const user: User = {
+        name: nameControl.value,
+        birthdate: birthdateControl.value,
+        email: emailControl.value,
+        phone: `${phoneControl.value}, +${phoneExtension?.value}`,
+        signature: this.signaturePad.toDataURL(),
+      };
+
+      this.form.markAllAsTouched();
+
+      if (this.form.valid && !this.signaturePad.isEmpty()) {
+        this.loading = true;
+
+        this.clearForm()
+
+        this.UsersService.postUser(user)
+          .subscribe(response => {
+            alert(response)
+            this.loading = false;
+          })
+      } else {
+        alert("Invalid form")
+        this.loading = false;
+      }
+    }
+  }
+
+  isValid(control: string, tipoError: string) {
+    return this.form.get(control)?.touched &&
+      this.form.get(control)?.getError(tipoError);
+  }
+
+  clearForm() {
+    this.form.reset();
+    this.signaturePad.clear();
   }
 
 }
