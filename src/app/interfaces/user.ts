b8cd11f2ef66506e824f6b5c2bc2bc04631525ef
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-17 14:31:18 -0600

    Created user service to post to database

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,2 +1,7 @@
 export interface User {
+    name: string,
+    birthdate: string,
+    email: string,
+    phone: string,
+    signature: string
 }
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ConfigurationService } from './configuration.service';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private _endpoints = {
+    user: '/user',
+  };
+
+  constructor(
+    private http: HttpClient,
+    private configurationService: ConfigurationService
+  ) { }
+
+  /**
+   * Method to create a new user in MongoDB
+   * 
+   * @param name 
+   * @param birthdate 
+   * @param email 
+   * @param phone 
+   * @param signature 
+   */
+  public postUser(user: User): Observable<User | null> {
+    if (user) {
+      const url = `${this.configurationService.apiBaseUrl}${this._endpoints.user}/new`;
+      const data = user;
+
+      return this.http.post<User | null>(url, data)
+    } else {
+      return of(null)
+    }
+  }
 }
